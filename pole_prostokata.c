3494a0e9a2ea343794892e92909df8cfaaec1fd1
Kamil Stodzierz <styxerm@gmail.com>
2024-01-19 11:37:09 +0100

    Dodanie_menu.

--- pole_prostokata.c
+++ pole_prostokata.c
@@ -7,7 +7,7 @@ float obliczPoleKola(float r) {
     return 3.14 * (r * r);
 }
 
-void menuPolaKola() {
+void select_PolaKola() {
     printf("\n Podaj promien kola\n");
     printf("r: ");
     scanf("%f", &r);
@@ -16,7 +16,7 @@ void menuPolaKola() {
 float obliczPoleProstokata(float a, float b){
     return (a*b);
 }
-void menuobliczPoleProstokata(){
+void select_PolaProstokata(){
     float a;
     float b;
     printf("Podaj wartosc a:\t");
@@ -26,12 +26,28 @@ void menuobliczPoleProstokata(){
 
     printf("\n wynik:%f",obliczPoleProstokata(a, b));
 }
-int main ()
-{
-
- menuobliczPoleProstokata();
- menuPolaKola();
-return 0;
+void menu() {
+    int wybor;
+    printf("prostokat czy kolo \n1. Prostokat\n2. Kolo\n");
+    scanf("%d", &wybor);
+    switch(wybor) {
+        case 1:
+            select_PolaProstokata();
+            break;
+        case 2:
+            select_PolaKola();
+            break;
+        default:
+            printf("Wybierz poprawn¹ liczbê!\n");
+            menu();
+            break;
+    }
 }
+
+int main() {
+    menu();
+    return 0;
+}
+
 
 
